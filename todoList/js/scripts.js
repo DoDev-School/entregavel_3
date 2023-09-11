350881051cb5f08daf907283593c4613a341478e
Luiz Pachioni <lopachioni@gmail.com>
2023-09-11 17:53:21 -0300

    ajustando repositorio

--- todoList/js/scripts.js
+++ todoList/js/scripts.js
@@ -1,85 +1,39 @@
 // Clase
 
 class ToDo {
-  Texto
-  Prioridade
-  Feito = false
-  constructor(texto, prioridade) {
-    this.Texto = texto
-    this.Prioridade = prioridade
-  }
+
 }
 
 // Array
-let arrayTodos = []
+
 
 //funções projeto
 
-function CriarToDo(texto, prioridade, array) {
-  let objetoTodo = new ToDo(texto, prioridade)
-  if (!array.some(x => x.Texto == texto))
-    array.push(objetoTodo)
-  return objetoTodo
+function CriarToDo() {
+
 }
 
-function AtualizarToDo(textoAntigo, textoNovo, array) {
-  let atualizado = false
-  array.forEach(todo => {
-    if (todo.Texto == textoAntigo) {
-      todo.Texto = textoNovo
-      atualizado = true
-    }
-  })
+function AtualizarToDo() {
 
-  return atualizado
 }
 
-function ConcluirToDo(array, texto) {
-  let concluido = false
-  array.forEach(todo => {
-    if (todo.Texto == texto) {
-      if (todo.Feito)
-        todo.Feito = false
-      else {
-        todo.Feito = true
-      }
-      concluido = true
-    }
-  })
-  return concluido
+function ConcluirToDo() {
+
 }
 
-function ExcluirToDo(array, texto) {
-  let index
-  let removido = false
-  array.forEach(todo => {
-    if (todo.Texto == texto) {
-      index = array.indexOf(todo)
-      removido = true
-    }
-  })
-  array.splice(index, 1)
-  return removido
+function ExcluirToDo() {
+
 }
 
-function PesquisarToDo(array, texto) {
-  let pesquisa = false
-  array.forEach(todo => {
-    if (todo.Texto.includes(texto)) {
-      pesquisa = true
-    }
-  })
-
-  return pesquisa
+function PesquisarToDo() {
+ 
 }
 
-function OrdenarCrescente(array) {
-  array.sort((a, b) => a.Prioridade - b.Prioridade)
-  return array
+function OrdenarCrescente() {
+  
 }
-function OrdenarDecrescente(array) {
-  array.sort((a, b) => b.Prioridade - a.Prioridade)
-  return array
+function OrdenarDecrescente() {
+  
 }
 
 // Seleção de elementos
